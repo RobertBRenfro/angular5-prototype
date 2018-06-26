de4e37f1bb5db4f31143cadf0a62482ca707e6e4
Robert Renfro <RRenfro@tsys.com>
2018-06-25 23:13:34 -0400

    added image animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import {trigger, animate, style, transition, animateChild, group, query, stagger} from '@angular/animations';
+import {HostBinding, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  styleUrls: ['./app.component.css'],
+  animations: [trigger('bgImgTrigger', [
+    transition('* <=> *', [
+      // this hides everything right away
+      query('*', style({ opacity: 0 }),{optional: true}),
+      
+      // starts to animate things with a stagger in between
+      query('*', stagger('300ms', [ animate('1s', 
+      style({ opacity: 1 }))]),{optional: true}),
+
+      query('*', [
+        style({ transform: 'translateX(200px)', opacity: 0 }),
+        stagger(100, [
+          animate('1200ms cubic-bezier(0.35, 0, 0.25, 1)', style('*'))
+        ])
+    ]),
+    query(
+      '*',
+      [
+        style({
+          opacity: 0,
+          transform: 'translateY(9rem) rotate(-10deg)'
+        }),
+        animate(
+          '0.35s cubic-bezier(0, 1.8, 1, 1.8)',
+          style({ opacity: 1, transform: 'translateY(0) rotate(0)' })
+        )
+  ])
+])]})
+
+
+@HostBinding('@preview')
+
 export class AppComponent {
   title = 'app';
 }
