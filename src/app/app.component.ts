f1707ee20546010e2fbb9c3ae9a19914a45097a3
Robert Renfro <rrenfro@tsys.com>
2018-06-27 10:30:22 -0600

    rotation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import {trigger, animate, style, transition, animateChild, group, query, stagger} from '@angular/animations';
 import {HostBinding, Input, Output, EventEmitter} from '@angular/core';
+import { statusChange } from './animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [trigger('bgImgTrigger', [
+  /*animations: [
+    statusChange
+  ]*/
+ animations: [trigger('bgImgTrigger', [
     transition('* <=> *', [
       // this hides everything right away
       query('*', style({ opacity: 0 }),{optional: true}),
@@ -27,15 +31,20 @@ import {HostBinding, Input, Output, EventEmitter} from '@angular/core';
       [
         style({
           opacity: 0,
-          transform: 'translateY(9rem) rotate(-10deg)'
+          transform: 'translateY(9rem) rotate(3600deg)'
         }),
         animate(
-          '0.35s cubic-bezier(0, 1.8, 1, 1.8)',
-          style({ opacity: 1, transform: 'translateY(0) rotate(60deg)' })
+          '15s cubic-bezier(0, 1.8, 1, 1.8)',
+          style({ opacity: 1, transform: 'translateY(0) rotate(360deg)' })
         )
   ])
-])])]})
+])]),
+trigger('statusChange', [transition('*<=>*', [    
+  query('*',  [style({opacity: 0, transform: 'translateY(9rem) rotate(36000deg)'}),
+    animate('0.60s cubic-bezier(0, 1.8, 1, 1.8)', style({ opacity: 1, transform: 'translateY(0) rotate(360deg)' }))])])])
+ ]})
 
 export class AppComponent {
   title = 'app';
+  statusState = 'loop-state';
 }
