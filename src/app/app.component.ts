06e952f5e7765518aa583fb343e9f105d941ec49
Robert Renfro <RRenfro@tsys.com>
2018-06-26 23:06:47 -0400

    more animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import {HostBinding, Input, Output, EventEmitter} from '@angular/core';
       query('*', style({ opacity: 0 }),{optional: true}),
       
       // starts to animate things with a stagger in between
-      query('*', stagger('300ms', [ animate('1s', 
+      query('*', stagger('30ms', [ animate('300ms', 
       style({ opacity: 1 }))]),{optional: true}),
 
       query('*', [
@@ -31,7 +31,7 @@ import {HostBinding, Input, Output, EventEmitter} from '@angular/core';
         }),
         animate(
           '0.35s cubic-bezier(0, 1.8, 1, 1.8)',
-          style({ opacity: 1, transform: 'translateY(0) rotate(0)' })
+          style({ opacity: 1, transform: 'translateY(0) rotate(60deg)' })
         )
   ])
 ])])]})
